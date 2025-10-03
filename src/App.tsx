20f683b4c8489daae59bb74a7b0c2efcf2d1657b
alexisborgesm-oss <alexisborgesm@gmail.com>
2025-10-03 12:58:46 -0700

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,3 @@
-// src/App.tsx
 import React, { useState } from 'react'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Navbar from '@/components/Navbar'
@@ -12,7 +11,6 @@ import './styles.css'
 
 export default function App() {
   const [user, setUser] = useState<User|null>(null)
-
   const logout = () => { localStorage.removeItem('user'); setUser(null) }
 
   const app = (
